bd0507dbc6e1f48b45030c6c94eb50aa60904d5a
juliacoelho3 <juliacoelho3@gmail.com>
2024-05-22 13:51:28 -0300

    Criaçao das entidades Endereeço e cliente

---
+++ grupo05api/src/main/java/br/gov/serratec/grupo05api/model/Endereco.java
@@ -0,0 +1,80 @@
+package br.gov.serratec.grupo05api.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+	private Long idEndereco;
+	private String cep;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private int numero;
+	private String complemento;
+	private String uf;
+
+	public Long getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Long idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+}
