bd28674712261fc1e92a2d8c699b7c26c12ce7d7
Pabullo-M <pabullo.muniz@gmail.com>
2024-05-21 22:47:22 -0300

    Criar entidade categoria e produto

---
+++ grupo05api/src/main/java/br/gov/serratec/grupo05api/Grupo05apiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.serratec.grupo05api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo05apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo05apiApplication.class, args);
+	}
+
+}
