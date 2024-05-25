72e9cee994c9bbd367f919417c5c70a042c99c33
Makrien <110492469+Makrien@users.noreply.github.com>
2024-05-25 18:46:06 -0300

    Tipo de imagem alterada para TEXT no sql

--- grupo05api/src/main/java/br/gov/serratec/grupo05api/dto/CategoriaDto.java
+++ grupo05api/src/main/java/br/gov/serratec/grupo05api/dto/CategoriaDto.java
@@ -1,15 +1,15 @@
 package br.gov.serratec.grupo05api.dto;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import br.gov.serratec.grupo05api.model.Categoria;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record CategoriaDto(
-        Long id,
-        
+        Long id,        
         @NotNull(message = "Nome não pode ser nulo")
         @NotEmpty(message = "Nome não pode ser vazio")
         @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
@@ -20,12 +20,17 @@ public record CategoriaDto(
         String descricao) {
 
     public Categoria toEntity() {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.convertValue(this, Categoria.class);
+        Categoria categoria = new Categoria();
+        categoria.setNome(this.nome);
+        categoria.setDescricao(this.descricao);
+        return categoria;
     }
 
     public static CategoriaDto toDto(Categoria categoriaEntity) {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.convertValue(categoriaEntity, CategoriaDto.class);
+        return new CategoriaDto(
+        			categoriaEntity.getId(),
+        			categoriaEntity.getNome(),
+        			categoriaEntity.getDescricao()
+        		);
     }
 }
