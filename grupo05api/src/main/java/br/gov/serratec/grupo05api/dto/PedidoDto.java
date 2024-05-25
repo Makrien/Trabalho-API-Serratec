94c234d815a46b949603a555122e74e625876f74
Makrien <110492469+Makrien@users.noreply.github.com>
2024-05-25 17:35:05 -0300

    Classes de pedidos completamente refatoradas

--- grupo05api/src/main/java/br/gov/serratec/grupo05api/dto/PedidoDto.java
+++ grupo05api/src/main/java/br/gov/serratec/grupo05api/dto/PedidoDto.java
@@ -1,11 +1,8 @@
 package br.gov.serratec.grupo05api.dto;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import br.gov.serratec.grupo05api.model.Cliente;
-import br.gov.serratec.grupo05api.model.ItemPedido;
 import br.gov.serratec.grupo05api.model.Pedido;
 
 public record PedidoDto(Long id,
@@ -14,35 +11,18 @@ public record PedidoDto(Long id,
         LocalDate dataEnvio,
         String status,
         Double valorTotal,
-        Long idCliente,
-        List<Long> idsItemPedido) {
+        Cliente cliente) {
 
-	public Pedido toEntity(Cliente cliente, List<ItemPedido> itens) {
-        Pedido pedido = new Pedido();
-        pedido.setDataPedido(this.dataPedido);
-        pedido.setDataEntrega(this.dataEntrega);
-        pedido.setDataEnvio(this.dataEnvio);
-        pedido.setStatus(this.status);
-        pedido.setValorTotal(this.valorTotal);
-        pedido.setCliente(cliente);
-        pedido.setItensPedido(itens);
-        return pedido;
-    }
+	public static PedidoDto toDto(Pedido pedidoEntity) {
+	    return new PedidoDto(
+	        pedidoEntity.getId(),
+	        pedidoEntity.getDataPedido(),
+	        pedidoEntity.getDataEntrega(),
+	        pedidoEntity.getDataEnvio(),
+	        pedidoEntity.getStatus(),
+	        pedidoEntity.getValorTotal(),
+	        pedidoEntity.getCliente()
+	    );
+	}
 
-    public static PedidoDto toDto(Pedido pedidoEntity) {
-        List<Long> itemIds = pedidoEntity.getItensPedido().stream()
-        		.map(ItemPedido::getId)
-        		.collect(Collectors.toList());
-        
-        return new PedidoDto(
-        		pedidoEntity.getId(),
-        		pedidoEntity.getDataPedido(),
-        		pedidoEntity.getDataEntrega(),
-        		pedidoEntity.getDataEnvio(),
-        		pedidoEntity.getStatus(),
-        		pedidoEntity.getValorTotal(),
-        		pedidoEntity.getCliente().getId(),
-        		itemIds
-        );
-    }
 }
--- grupo05api/src/main/java/br/gov/serratec/grupo05api/service/PedidoService.java
+++ grupo05api/src/main/java/br/gov/serratec/grupo05api/service/PedidoService.java
@@ -9,12 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import br.gov.serratec.grupo05api.dto.PedidoCadastroDto;
 import br.gov.serratec.grupo05api.dto.PedidoDto;
 import br.gov.serratec.grupo05api.model.Cliente;
-import br.gov.serratec.grupo05api.model.ItemPedido;
 import br.gov.serratec.grupo05api.model.Pedido;
 import br.gov.serratec.grupo05api.repository.ClienteRepository;
-import br.gov.serratec.grupo05api.repository.ItemPedidoRepository;
 import br.gov.serratec.grupo05api.repository.PedidoRepository;
 import jakarta.validation.Valid;
 
@@ -26,57 +25,52 @@ public class PedidoService {
 	
 	@Autowired
 	private ClienteRepository clienteRepo;
-	
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepo;
 
 	public List<PedidoDto> obterTodos() {
-		return pedidoRepo.findAll().stream().map(p -> PedidoDto.toDto(p)).toList();
+		return pedidoRepo.findAll().stream()
+				.map(PedidoDto::toDto)
+				.collect(Collectors.toList());
+	}
+	
+	public Optional<PedidoDto> obterPorId(Long id) {
+		Optional<Pedido> pedidoEntity = pedidoRepo.findById(id);
+		if (pedidoEntity.isEmpty()) {
+			return Optional.empty();
+		}
+		return Optional.of(PedidoDto.toDto(pedidoEntity.get()));
 	}
 
-	public PedidoDto cadastrar(@Valid PedidoDto novoPedido) {
+	public PedidoDto cadastrar(@Valid PedidoCadastroDto novoPedido) {
 		Optional<Cliente> clienteOpt = clienteRepo.findById(novoPedido.idCliente());
 		if (clienteOpt.isEmpty()) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente não cadastrado.");
 		}
 		
-		List<ItemPedido> itens = novoPedido.idsItemPedido().stream()
-				.map(id -> itemPedidoRepo.findById(id).orElseThrow(() ->
-					new ResponseStatusException(HttpStatus.BAD_REQUEST, "Item não cadastrado para registro em pedido.")))
-				.collect(Collectors.toList());
-		
-		Pedido pedido = novoPedido.toEntity(clienteOpt.get(), itens);
-		
+		Pedido pedido = novoPedido.toEntity(clienteOpt.get());		
 		Pedido pedidoEntity = pedidoRepo.save(pedido);
 		return PedidoDto.toDto(pedidoEntity);
 	}
 
-	public Optional<PedidoDto> atualizar(Long pedidoId, @Valid PedidoDto pedidoDto) {
+	public Optional<PedidoDto> atualizar(Long pedidoId, @Valid PedidoCadastroDto pedidoDto) {
 		if (pedidoRepo.existsById(pedidoId)) {
 			Optional<Cliente> clienteOpt = clienteRepo.findById(pedidoDto.idCliente());
 			if (clienteOpt.isEmpty()) {
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente não cadastrado.");
 			}
-			
-			List<ItemPedido> itens = pedidoDto.idsItemPedido().stream()
-				.map(itemId -> itemPedidoRepo.findById(itemId).orElseThrow(() -> 
-					new ResponseStatusException(HttpStatus.BAD_REQUEST, "Item não cadastrado para registro em pedido.")))
-				.collect(Collectors.toList());
 		
-				Pedido pedidoEntity = pedidoRepo.findById(pedidoId).orElseThrow(() -> 
-					new ResponseStatusException(HttpStatus.NOT_FOUND, "Pedido não encontrado"));
-				
-				pedidoEntity.setDataPedido(pedidoDto.dataPedido());
-	            pedidoEntity.setDataEntrega(pedidoDto.dataEntrega());
-	            pedidoEntity.setDataEnvio(pedidoDto.dataEnvio());
-	            pedidoEntity.setStatus(pedidoDto.status());
-	            pedidoEntity.setValorTotal(pedidoDto.valorTotal());
-	            pedidoEntity.setCliente(clienteOpt.get());
-	            pedidoEntity.setItensPedido(itens);
+			Pedido pedidoEntity = pedidoRepo.findById(pedidoId).orElseThrow(() -> 
+				new ResponseStatusException(HttpStatus.NOT_FOUND, "Pedido não encontrado"));
+			
+			pedidoEntity.setDataPedido(pedidoDto.dataPedido());
+            pedidoEntity.setDataEntrega(pedidoDto.dataEntrega());
+            pedidoEntity.setDataEnvio(pedidoDto.dataEnvio());
+            pedidoEntity.setStatus(pedidoDto.status());
+            pedidoEntity.setValorTotal(pedidoDto.valorTotal());
+            pedidoEntity.setCliente(clienteOpt.get());
 
-	            pedidoRepo.save(pedidoEntity);
-	            return Optional.of(PedidoDto.toDto(pedidoEntity));
-	        }
+            pedidoRepo.save(pedidoEntity);
+            return Optional.of(PedidoDto.toDto(pedidoEntity));
+		}
 	        return Optional.empty();
 	}
 
@@ -92,13 +86,7 @@ public class PedidoService {
 		return true;
 	}
 
-	public Optional<PedidoDto> obterPorId(Long id) {
-		Optional<Pedido> pedidoEntity = pedidoRepo.findById(id);
-		if (pedidoEntity.isEmpty()) {
-			return Optional.empty();
-		}
-		return Optional.of(PedidoDto.toDto(pedidoEntity.get()));
-	}
+
 	
 	
 }
